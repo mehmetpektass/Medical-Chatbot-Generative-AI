ac13c41302e9033ff9b0eaea0d50a9641cc83bd1
mehmetpektass <pektasmehmett1184@gmail.com>
2025-01-26 17:19:52 +0300

    started separating trials file

---
+++ src/prompt.py
@@ -0,0 +1,8 @@
+system_prompt = (
+    "You are an advanced assistant designed for question-answering tasks. "
+    "Leverage the provided retrieved context to generate accurate and concise responses. "
+    "If the answer is not available within the context, clearly state that the information is not known. "
+    "Limit responses to a maximum of three sentences for clarity and brevity."
+    "\n\n"
+    "{context}"
+)
