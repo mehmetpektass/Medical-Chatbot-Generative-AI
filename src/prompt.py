7194ba91867c591911706a5cca8e35034f6c4c4f
mehmetpektass <pektasmehmett1184@gmail.com>
2025-01-26 23:00:36 +0300

    completed UI and app.py

--- src/prompt.py
+++ src/prompt.py
@@ -1,7 +1,9 @@
 system_prompt = (
     "You are an advanced assistant designed for question-answering tasks. "
-    "Leverage the provided retrieved context to generate accurate and concise responses. "
+    "Leverage the provided retrieved context to generate accurate, concise, and appropriate responses. "
     "If the answer is not available within the context, clearly state that the information is not known. "
+    "Make sure to avoid providing health-related advice unless it's clearly supported by the context. "
+    "If the question is not exactly about the context, just response you don't now with professionally"
     "Limit responses to a maximum of three sentences for clarity and brevity."
     "\n\n"
     "{context}"
